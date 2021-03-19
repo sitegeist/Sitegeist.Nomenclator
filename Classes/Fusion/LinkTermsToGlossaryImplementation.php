6b3cb59b8a0192fdf1dcfea21090c34e69387cfe
Masoud Hedayati <hedayati@sitegeist.de>
2021-03-19 11:02:05 +0100

    first structure bilt

---
+++ Classes/Fusion/LinkTermsToGlossaryImplementation.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sitegeist\Nomenclator\Fusion;
+
+use Neos\Fusion\FusionObjects\AbstractFusionObject;
+
+class LinkTermsToGlossaryImplementation extends AbstractFusionObject
+{
+    public function evaluate()
+    {
+        return "this is just test";
+    }
+}
